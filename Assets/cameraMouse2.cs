d0e955badddb218b161312b8c3d8a74ab7dcb0e7
ilia <iliaraysin@gmail.com>
2017-05-06 12:30:22 +0300

    modifying the fbx model

--- Assets/cameraMouse2.cs
+++ Assets/cameraMouse2.cs
@@ -26,8 +26,8 @@ public class cameraMouse2 : MonoBehaviour {
 	public float sensitivityY = 15F;
 	public float minimumX = -360F;
 	public float maximumX = 360F;
-	public float minimumY = -60F;
-	public float maximumY = 60F;
+	public float minimumY = -1800F;
+	public float maximumY = 1800F;
 	float rotationX = 0F;
 	float rotationY = 0F;
 	Quaternion originalRotation;
