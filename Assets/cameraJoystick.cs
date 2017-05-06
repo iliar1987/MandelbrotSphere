165d39145d092fee35e8ea63ecca66ea5eda5938
ilia <iliaraysin@gmail.com>
2017-05-06 18:42:49 +0300

    added cameraJoystick script

---
+++ Assets/cameraJoystick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraJoystick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotx = Input.GetAxis ("CamRotUp");
+		float roty = Input.GetAxis ("CamRotLeft");
+		if (rotx != 0 || roty != 0) {
+			gameObject.transform.Rotate (new Vector3 (rotx, roty, 0));
+		}
+
+		
+	}
+}
