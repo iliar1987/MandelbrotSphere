b71779a98a47d45f130b4181aea9875ad446e605
ilia <iliaraysin@gmail.com>
2017-05-31 15:20:26 +0300

    seems to have fixed it. now making conversion back to float

--- CudaMandelbrot/HighPrecision1/FP128.cuh
+++ CudaMandelbrot/HighPrecision1/FP128.cuh
@@ -38,30 +38,57 @@ public:
 		: lo(p.first), hi(p.second)
 	{}
 
+	__host__ __device__ explicit operator float() const
+	{
+		return 0.0; //__ffs
+	}
+
+	__host__ __device__ explicit CFixedPoint128(const float d)
+	{
+		/*assert(d < 4 && d >= -4);*/
+		// from wikipedia:
+		// (-1) ** b31  *  ( 1.b22b21b20...b0)_2  *  2 ** ((b30b29...b23)_2 - 127)
+		const uint32_t &x = *(reinterpret_cast<const uint32_t*>(&d));
+		uint32_t e = (x & 0x7f800000) >> 23;
+		uint32_t f = x & 0x007fffff;
+		uint32_t s = x >> 31;
+		lolo = f | 0x800000;
+		lohi = 0;
+		hi = 0;
+		int shft = (int)e + (-127 + 125 - 23);
+		if (shft > 0)
+			operator <<=(shft);
+		else if (shft < 0)
+			operator >>=(shft);
+
+		if (s)
+			Negate();
+//#ifndef __CUDA_ARCH__
+//		printf("%f = %d\t%d\t%d\n", d, f, e - 127, s);
+//#endif
+	}
+
 	__device__ __forceinline__ CFixedPoint128 operator * (const CFixedPoint128& other) const
 	{
-
 		CFixedPoint128 result;
-		asm("{\n\t"
-
-			"mul.hi.u64     %0, %2, %5		;\n\t"  /* (a0 * b1)_hi */
-
-			"mad.hi.cc.u64  %0, %3,  %4, %0;\n\t"  /* (a1 * b0)_hi */
-
-			"madc.hi.u64     %1, %3, %5,  0;\n\t"  /* (a1 * b1)_hi */
-
-			"mad.lo.cc.u64   %0, %3, %5, %0;\n\t"  /* (a1 * b1)_lo */
-
-			"addc.u64		 %1, %1,	0	;\n\t"  /* propagate carry */
-
+		uint64_t p1;
+		asm ("{\n\t"
+			".reg .u64 %2					;\n\t" //declare p1 register
+			"mul.hi.u64		%2,	%3,	%5		;\n\t" // p1 = (a0 * b0)_hi
+			"mad.lo.cc.u64		%2,	%3,	%6,	%2	;\n\t" // p1 += (a0 * b1)_lo -> C
+			"madc.hi.u64     %0, %3, %6,  0		;\n\t" // p2 = (a0 * b1)_hi + C
+			"mad.lo.cc.u64		%2,	%4,	%5,	%2	;\n\t" // p1 += (a1 * b0)_lo -> C
+			"madc.hi.cc.u64  %0, %4,  %5, %0;\n\t"  // p2 += (a1 * b0)_hi + C -> C
+			"madc.hi.u64     %1, %4, %6,  0;\n\t"  // p3 = (a1 * b1)_hi + C
+			"mad.lo.cc.u64   %0, %4, %6, %0;\n\t"  // p2 += (a1 * b1)_lo -> C 
+			"addc.u64		 %1, %1,	0	;\n\t"  // p3 += C
 			"}"
-
-			: "=l"(result.lo), "=l"(result.hi)
-
+			: "=l"(result.lo), "=l"(result.hi), "=l"(p1)
 			: "l"(lo), "l"(hi), "l"(other.lo), "l"(other.hi));
-		
-		// todo: big problem. we're losing data in each multiplication.
-		result <<= 3 ;
+
+		// multiply by 8 and add from %2
+		result <<= 3;
+		result.lo |= (p1 << 61); //add lower 3 bits from %2.
 
 		return result;
 	}
@@ -84,7 +111,7 @@ public:
 		return *this;
 	}
 
-	__device__ __host__ __forceinline__ CFixedPoint128 & operator <<= (const unsigned int n)
+	__device__ __host__ CFixedPoint128 & operator <<= (const unsigned int n)
 	{
 		if (n >= 64)
 		{
@@ -98,12 +125,12 @@ public:
 			const uint64_t mask = s_allOnes << (64 - n);
 			const uint64_t carry = (lo & mask);
 			lo <<= n;
-			hi |= (carry >> (64 - 2 * n));
+			hi |= (carry >> (64 - n));
 		}
 		return *this;
 	}
 
-	__device__ __host__ __forceinline__ CFixedPoint128 & operator >>= (const unsigned int n)
+	__device__ __host__ CFixedPoint128 & operator >>= (const unsigned int n)
 	{
 		const bool bNeg = IsNeg();
 		if (n >= 64)
@@ -120,7 +147,7 @@ public:
 			const uint64_t mask = s_allOnes >> (64 - n);
 			const uint64_t carry = (hi & mask);
 			hi >>= n;
-			lo |= (carry << (64 - 2 * n));
+			lo |= (carry << (64 - n)); //TODO: is it n or 2n?
 			if (bNeg)
 			{
 				hi |= (s_allOnes << (64 - n));
@@ -130,7 +157,7 @@ public:
 		return *this;
 	}
 
-	__device__ __host__ __forceinline__ void Negate()
+	__device__ __host__ void Negate()
 	{
 #ifndef __CUDA_ARCH__
 		hi = ~hi;
@@ -148,36 +175,6 @@ public:
 			"add.cc.u64 %0, %0, 1 ; \n\t"
 			"addc.u64 %1, %1, 0; \n\t"
 			"}" : "+l"(lo), "+l"(hi));
-#endif
-	}
-
-	__host__ __device__  __forceinline__ explicit operator float() const
-	{
-		return 0.0; //__ffs
-	}
-
-	__host__ __device__ explicit CFixedPoint128(const float d)
-	{
-		/*assert(d < 4 && d >= -4);*/
-		// from wikipedia:
-		// (-1) ** b31  *  ( 1.b22b21b20...b0)_2  *  2 ** ((b30b29...b23)_2 - 127)
-		const uint32_t &x = *(reinterpret_cast<const uint32_t*>(&d));
-		uint32_t e = (x & 0x7f800000) >> 23;
-		uint32_t f = x & 0x007fffff;
-		uint32_t s = x >> 31;
-		lolo = f | 0x800000;
-		lohi = 0;
-		hi = 0;
-		int shft = (int)e + (- 127 + 125 - 24);
-		if (shft > 0)
-			operator <<=(shft);
-		else if (shft < 0)
-			operator >>=(shft);
-
-		if (s)
-			Negate();
-#ifndef __CUDA_ARCH__
-		printf("%f = %d\t%d\t%d\n", d,f, e-127, s);
 #endif
 	}
 
